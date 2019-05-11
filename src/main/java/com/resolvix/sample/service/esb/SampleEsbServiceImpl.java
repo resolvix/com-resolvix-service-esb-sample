32ed5f03d6c98b3145263f7aa73d1097c8701e4b
Roger Bisson <rwbisson@resolvix.com>
2019-05-12 00:15:00 +0100

    Revised approach to service request fault handling

--- src/main/java/com/resolvix/sample/service/esb/SampleEsbServiceImpl.java
+++ src/main/java/com/resolvix/sample/service/esb/SampleEsbServiceImpl.java
@@ -1,10 +1,24 @@
 package com.resolvix.sample.service.esb;
 
 import com.resolvix.lib.service.BaseEsbServiceImpl;
+import com.resolvix.lib.service.api.ServiceFault;
+import com.resolvix.service.soa.*;
 
 public class SampleEsbServiceImpl
     extends BaseEsbServiceImpl
+    implements SamplePortType
 {
+    @Override
+    public SampleResponse sampleOperation(SampleRequest request)
+        throws SampleFaultOne, SampleFaultTwo {
+        return null;
+    }
 
-
+//    @Override
+//    public SampleResponse sampleOperation(SampleRequest request)
+//    {
+//        SampleEsbServiceRequestHandlerImpl requestHandler
+//            = new SampleEsbServiceRequestHandlerImpl();
+//        return requestHandler.execute(request);
+//    }
 }
--- src/main/java/com/resolvix/sample/service/esb/SampleEsbServiceRequestHandlerImpl.java
+++ src/main/java/com/resolvix/sample/service/esb/SampleEsbServiceRequestHandlerImpl.java
@@ -1,21 +1,49 @@
 package com.resolvix.sample.service.esb;
 
+import com.google.common.collect.Maps;
 import com.resolvix.lib.service.BaseEsbServiceRequestHandlerImpl;
+import com.resolvix.lib.service.ServiceFaultMap;
 import com.resolvix.lib.service.api.ServiceException;
 import com.resolvix.lib.service.api.ServiceFault;
+import com.resolvix.lib.service.api.ServiceFaultMaplet;
+import com.resolvix.sample.service.esb.exception.SampleFaultOne;
+import com.resolvix.sample.service.esb.exception.SampleFaultTwo;
 import com.resolvix.service.soa.SampleRequest;
 import com.resolvix.service.soa.SampleResponse;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class SampleEsbServiceRequestHandlerImpl
     extends BaseEsbServiceRequestHandlerImpl<
         SampleRequest,
         SampleResponse,
-        ServiceFault,
         ProcessingContext>
 {
 
+    private static final List<ServiceFaultMaplet<?, ?, ?>> SERVICE_FAULT_MAPLETS = Arrays.asList(
+        ServiceFaultMaplet.of(SampleFaultOne.class, SampleEsbServiceRequestHandlerImpl::toSampleFaultOne),
+        ServiceFaultMaplet.of(SampleFaultTwo.class, SampleEsbServiceRequestHandlerImpl::toSampleFaultTwo));
+
+    private static final ServiceFaultMap SERVICE_FAULT_MAP
+        = ServiceFaultMap.of(SERVICE_FAULT_MAPLETS);
+
     public SampleEsbServiceRequestHandlerImpl() {
         //
+    }
+
+    private static com.resolvix.service.soa.SampleFaultOne toSampleFaultOne(
+        ProcessingContext processingContext,
+        SampleFaultOne serviceFaultOne) {
+        return null;
+    }
+
+    private static com.resolvix.service.soa.SampleFaultTwo toSampleFaultTwo(
+        ProcessingContext processingContext,
+        SampleFaultTwo serviceFaultTwo) {
+        return null;
     }
 
     @Override
@@ -44,5 +72,12 @@ public class SampleEsbServiceRequestHandlerImpl
         throws ServiceFault
     {
         return null;
+    }
+
+    //@Override
+    protected <E extends Exception> E fault(ProcessingContext processingContext, ServiceFault sf)
+        throws Exception
+    {
+        return SERVICE_FAULT_MAP.map(processingContext, sf) ;
     }
 }
