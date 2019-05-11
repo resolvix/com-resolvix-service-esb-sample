a23a5867d1d7d491498dc89d31465d03e222751d
Roger Bisson <rwbisson@resolvix.com>
2019-05-12 00:36:57 +0100

    Refactoring etc

--- src/main/java/com/resolvix/sample/service/esb/SampleEsbServiceImpl.java
+++ src/main/java/com/resolvix/sample/service/esb/SampleEsbServiceImpl.java
@@ -8,17 +8,15 @@ public class SampleEsbServiceImpl
     extends BaseEsbServiceImpl
     implements SamplePortType
 {
+
     @Override
     public SampleResponse sampleOperation(SampleRequest request)
-        throws SampleFaultOne, SampleFaultTwo {
-        return null;
+        throws SampleFaultOne, SampleFaultTwo
+    {
+        try {
+            return execute(SampleEsbServiceRequestHandlerImpl.class, request);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-
-//    @Override
-//    public SampleResponse sampleOperation(SampleRequest request)
-//    {
-//        SampleEsbServiceRequestHandlerImpl requestHandler
-//            = new SampleEsbServiceRequestHandlerImpl();
-//        return requestHandler.execute(request);
-//    }
 }
