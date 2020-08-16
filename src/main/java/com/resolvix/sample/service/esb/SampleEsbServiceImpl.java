0cce6f61590fd58ef787a1f010bd8090bed496bd
Roger Bisson <rwbisson@resolvix.com>
2020-08-17 00:53:55 +0100

    Added beans.xml; added @WebService annotation etc

--- src/main/java/com/resolvix/sample/service/esb/SampleEsbServiceImpl.java
+++ src/main/java/com/resolvix/sample/service/esb/SampleEsbServiceImpl.java
@@ -3,6 +3,12 @@ package com.resolvix.sample.service.esb;
 import com.resolvix.lib.service.esb.BaseEsbServiceImpl;
 import com.resolvix.service.soa.*;
 
+import javax.jws.WebService;
+
+@WebService(
+    serviceName = "SampleEsbService",
+    portName = "SamplePortType",
+    endpointInterface = "com.resolvix.service.soa.SampleService")
 public class SampleEsbServiceImpl
     extends BaseEsbServiceImpl
     implements SamplePortType
